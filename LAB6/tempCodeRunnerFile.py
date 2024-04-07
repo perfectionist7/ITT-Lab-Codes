6b2f43036a4ca0b15d773736408f113f7e81b06e
perfectionist7 <ayushkhandelwal@devot.ai>
2024-04-07 23:16:45 +0530

    changes:

---
+++ LAB6/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# Python program to mail merger
+# Names are in the file names.txt
+# Body of the mail is in body.txt
+
+# open names.txt for reading
+with open("names.txt", "r", encoding="utf-8") as names_file:
